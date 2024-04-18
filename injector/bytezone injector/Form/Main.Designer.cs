ac776e5f01ef89efece54bdfd80a30d7b980e292
Musaed <106308342+Mes2d@users.noreply.github.com>
2024-04-17 22:21:13 -0700

    Update 1.2

---
+++ injector/bytezone injector/Form/Main.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ByteZone_Injector
+{
+    partial class ByteZone_Injector
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ByteZone_Injector));
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            label2 = new Label();
+            button2 = new Button();
+            dllNameLbl = new Label();
+            label3 = new Label();
+            listView1 = new ListView();
+            imageList1 = new ImageList(components);
+            sProcessLbl = new Label();
+            panel1 = new Panel();
+            label4 = new Label();
+            panel2 = new Panel();
+            wLbl = new Label();
+            closeAfterInjectCheckBox = new CheckBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.ForeColor = Color.White;
+            label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { resources.GetString("comboBox1.Items"), resources.GetString("comboBox1.Items1"), resources.GetString("comboBox1.Items2"), resources.GetString("comboBox1.Items3"), resources.GetString("comboBox1.Items4") });
+            resources.ApplyResources(comboBox1, "comboBox1");
+            comboBox1.Name = "comboBox1";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(20, 106, 94);
+            button1.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(button1, "button1");
+            button1.ForeColor = Color.White;
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.ForeColor = Color.White;
+            label2.Name = "label2";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(20, 106, 94);
+            button2.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(button2, "button2");
+            button2.ForeColor = Color.White;
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // dllNameLbl
+            // 
+            resources.ApplyResources(dllNameLbl, "dllNameLbl");
+            dllNameLbl.ForeColor = Color.White;
+            dllNameLbl.Name = "dllNameLbl";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.ForeColor = Color.White;
+            label3.Name = "label3";
+            // 
+            // listView1
+            // 
+            listView1.BackColor = Color.FromArgb(35, 44, 51);
+            listView1.ForeColor = Color.White;
+            resources.ApplyResources(listView1, "listView1");
+            listView1.Name = "listView1";
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.SmallIcon;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            resources.ApplyResources(imageList1, "imageList1");
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // sProcessLbl
+            // 
+            resources.ApplyResources(sProcessLbl, "sProcessLbl");
+            sProcessLbl.ForeColor = Color.White;
+            sProcessLbl.Name = "sProcessLbl";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(35, 44, 51);
+            panel1.Controls.Add(dllNameLbl);
+            panel1.Controls.Add(sProcessLbl);
+            resources.ApplyResources(panel1, "panel1");
+            panel1.Name = "panel1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.ForeColor = Color.White;
+            label4.Name = "label4";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(35, 44, 51);
+            panel2.Controls.Add(wLbl);
+            panel2.Controls.Add(closeAfterInjectCheckBox);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(label1);
+            resources.ApplyResources(panel2, "panel2");
+            panel2.Name = "panel2";
+            panel2.Paint += panel2_Paint;
+            // 
+            // wLbl
+            // 
+            resources.ApplyResources(wLbl, "wLbl");
+            wLbl.ForeColor = Color.Red;
+            wLbl.Name = "wLbl";
+            // 
+            // closeAfterInjectCheckBox
+            // 
+            resources.ApplyResources(closeAfterInjectCheckBox, "closeAfterInjectCheckBox");
+            closeAfterInjectCheckBox.ForeColor = Color.White;
+            closeAfterInjectCheckBox.Name = "closeAfterInjectCheckBox";
+            closeAfterInjectCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ByteZone_Injector
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(34, 41, 49);
+            Controls.Add(panel2);
+            Controls.Add(label4);
+            Controls.Add(panel1);
+            Controls.Add(listView1);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "ByteZone_Injector";
+            Opacity = 0.88D;
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Label label2;
+        private Button button2;
+        private Label dllNameLbl;
+        private Label label3;
+        private ListView listView1;
+        private ImageList imageList1;
+        private Label sProcessLbl;
+        private Panel panel1;
+        private Label label4;
+        private Panel panel2;
+        private CheckBox closeAfterInjectCheckBox;
+        private Label wLbl;
+    }
+}
---
+++ injector/bytezone injector/Form/Main.cs
@@ -0,0 +1,205 @@
+using System.Diagnostics;
+using System.Drawing;
+namespace ByteZone_Injector
+{
+
+
+
+
+    public partial class ByteZone_Injector : Form
+    {
+
+        /// <summary>
+        /// Gets or sets the name of the target process for injection.
+        /// </summary>
+        public string S_ProcessName { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the file path of the DLL to be injected.
+        /// </summary>
+        private string dllPath;
+        private string dllBuffer;
+        /// <summary>
+        /// Instance of the InjectionMethods class used for performing injections.
+        /// </summary>
+        private InjectionMethods I_Method;
+
+        private ImageList imageList = new ImageList();
+        public ByteZone_Injector()
+        {
+            InitializeComponent();
+            wLbl.Visible = false;
+            I_Method = new InjectionMethods();
+        }
+
+
+        /// <summary>
+        /// Retrieves a list of currently running processes and populates the process list.
+        /// </summary>
+        private void GetProcesses()
+        {
+            Process[] processes = Process.GetProcesses(); // Get a list of all running processes
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    if (process.MainModule != null)
+                    {
+                        Icon processIcon = Icon.ExtractAssociatedIcon(process.MainModule.FileName);
+                        imageList.Images.Add(process.MainModule.FileName, processIcon); // Access the Images property of the imageList object
+                        ListViewItem item = new ListViewItem(process.ProcessName, process.MainModule.FileName);
+                        listView1.Items.Add(item);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Some of windows processes does not have icon this will through exception foreach process he can't get the icon so leave it
+                    //MessageBox.Show($"Error extracting icon for process {process.ProcessName}: {ex.Message}");
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.SmallImageList = imageList;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+
+            Thread thread = new Thread(GetProcesses); // Call GetProcesses in new thread to improve performance 
+            thread.Start(); // Start the thread 
+
+
+            //GetProcesses(); // Old method take a while until load the injector 
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(S_ProcessName) && !string.IsNullOrEmpty(dllPath))
+            {
+                // Check the ComboBox selection and call the corresponding injection method
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    I_Method.NtNativeInjection(S_ProcessName, dllPath);
+                    if (closeAfterInjectCheckBox.Checked)
+                    {
+                        Environment.Exit(0); // If Close after injection checked Call env exit
+                    }
+                }
+
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    I_Method.NtManualMapInjection(S_ProcessName, dllPath);
+                    if (closeAfterInjectCheckBox.Checked)
+                    {
+                        Environment.Exit(0); // If Close after injection checked Call env exit
+                    }
+                }
+
+                else if (comboBox1.SelectedIndex == 2) 
+                {
+                    // Kernel 
+                }
+
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    I_Method.NtThreadHijacking(S_ProcessName, dllPath);
+
+                    if (closeAfterInjectCheckBox.Checked) // If Close after injection checked Call env exit
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+                else if (comboBox1.SelectedIndex == 4)
+                {
+                    I_Method.NtLdrpLoadDll(S_ProcessName, dllPath);
+                    
+                    if (closeAfterInjectCheckBox.Checked) // If CLose after injection checked Call env exit
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a process and a DLL file first.");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 4)
+            {
+                wLbl.Visible = true;
+            }
+            else
+            {
+                wLbl.Visible = false;
+            }
+        }
+
+        /// <summary>
+        /// Handles the button click event to select a DLL file using an OpenFileDialog.
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = Environment.CurrentDirectory; // Set the initial directory to the current directory
+
+            ofd.Filter = "dll files (*.dll) | *.dll"; // Filter to display only DLL files
+
+            // If the user selects a file and clicks OK
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                dllPath = ofd.FileName; // Get the selected file path
+
+
+                if (!string.IsNullOrEmpty(dllPath)) // If the file path is not empty
+                {
+
+                    string fileName = Path.GetFileName(dllPath); // Get the file name without the path
+                    dllNameLbl.Text = "Selected module: " + fileName;
+                }
+            }
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Handles the selection change event of the list box to update the selected process name.
+        /// </summary>
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                S_ProcessName = selectedItem.SubItems[0].Text;
+                sProcessLbl.Text = "Selected Process: " + selectedItem.SubItems[0].Text;
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
