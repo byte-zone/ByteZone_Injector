2f86e9620745b8237797ddc8784eab2e7e66ca79
FastXSkyline <164813358+FastXSkyline@users.noreply.github.com>
2024-04-18 06:00:35 +0000

    Update dllmain.cpp

--- Dll_Example/Dll_Test/dllmain.cpp
+++ Dll_Example/Dll_Test/dllmain.cpp
@@ -9,7 +9,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        MessageBoxA(NULL, "You didn't fail in non page area!", "Boo", MB_OK);
+        MessageBoxA(NULL, "You didn't fail in non page area !", "Boo", MB_OK);
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
