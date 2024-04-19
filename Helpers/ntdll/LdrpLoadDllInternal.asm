8219d3d47fa3510e81662c60dde7031f9586bdb3
Musaed <106308342+Mes2d@users.noreply.github.com>
2024-04-19 14:43:27 -0700

    Update LdrpLoadDllInternal.asm

--- Helpers/ntdll/LdrpLoadDllInternal.asm
+++ Helpers/ntdll/LdrpLoadDllInternal.asm
@@ -1,1 +1,121 @@
+; Pseudocode for LdrpLoadDllInternal function from ntdll
+__int64 __fastcall LdrpLoadDllInternal(
+        __int64 a1,
+        int a2,
+        unsigned int a3,
+        int a4,
+        __int64 a5,
+        __int64 a6,
+        __int64 *a7,
+        int *a8)
+{
+  __int64 result; // rax
+  int *v12; // rbx
+  char v13; // di
+  int v14; // eax
+  __int64 v15; // rdx
+  __int64 v16; // rax
+  int v17; // eax
+  int v18; // eax
+  __int64 v19; // [rsp+28h] [rbp-50h]
+  __int64 v20; // [rsp+48h] [rbp-30h] BYREF
 
+  if ( (LdrpDebugFlags & 9) != 0 )
+    LdrpLogDbgPrint(
+      (unsigned int)"minkernel\\ntdll\\ldrapi.c",
+      425,
+      (unsigned int)"LdrpLoadDllInternal",
+      3,
+      "DLL name: %wZ\n",
+      a1);
+  *a7 = 0LL;
+  v20 = 0LL;
+  result = LdrpFastpthReloadedDll(a1, a3, a6, a7);
+  if ( (int)result < 0 )
+  {
+    if ( (NtCurrentTeb()->SameTebFlags & 0x1000) != 0 )
+    {
+      v13 = 1;
+    }
+    else
+    {
+      v13 = 0;
+      LdrpDrainWorkQueue(0LL);
+    }
+    if ( !a6 || v13 || *(_DWORD *)(*(_QWORD *)(a6 + 152) + 24LL) )
+    {
+      LdrpDetectDetour();
+      v12 = a8;
+      v14 = LdrpFindOrPrepareLoadingModule(a1, a2, a3, a4, a5, (__int64)&v20, (__int64)a8);
+      if ( v14 == -1073741515 )
+      {
+        LOBYTE(v15) = 1;
+        LdrpProcessWork(*(_QWORD *)(v20 + 176), v15);
+      }
+      else if ( v14 != -1073741267 && v14 < 0 )
+      {
+        *a8 = v14;
+      }
+    }
+    else
+    {
+      v12 = a8;
+      *a8 = -1073741515;
+    }
+    result = LdrpDrainWorkQueue(1LL);
+    if ( v20 )
+    {
+      v16 = LdrpHandleReplacedModule();
+      *a7 = v16;
+      if ( v20 != v16 )
+      {
+        LdrpFreeReplacedModule();
+        v20 = *a7;
+      }
+      if ( *(_QWORD *)(v20 + 176) )
+        LdrpCondenseGraph(*(_QWORD *)(v20 + 152));
+      if ( *v12 >= 0 )
+      {
+        v17 = LdrpPrepareModuleForExecution(v20, v12);
+        *v12 = v17;
+        if ( v17 >= 0 )
+        {
+          v18 = LdrpBuildForwarderLink(a6, v20);
+          *v12 = v18;
+          if ( v18 >= 0 && !LdrInitState )
+            LdrpPinModule(v20);
+        }
+      }
+      result = LdrpFreeLoadContextOfNode(*(_QWORD *)(v20 + 152), v12);
+      if ( *v12 < 0 )
+      {
+        *a7 = 0LL;
+        LdrpDecrementModuleLoadCountEx(v20, 0LL);
+        result = LdrpDereferenceModule(v20);
+      }
+    }
+    else
+    {
+      *v12 = -1073741801;
+    }
+    if ( !v13 )
+      result = LdrpDropLastInProgressCount();
+  }
+  else
+  {
+    v12 = a8;
+    *a8 = result;
+  }
+  if ( (LdrpDebugFlags & 9) != 0 )
+  {
+    LODWORD(v19) = *v12;
+    return LdrpLogDbgPrint(
+             (unsigned int)"minkernel\\ntdll\\ldrapi.c",
+             655,
+             (unsigned int)"LdrpLoadDllInternal",
+             4,
+             "Status: 0x%08lx\n",
+             v19);
+  }
+  return result;
+}
